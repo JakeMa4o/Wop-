5520922984b2f090c84fe67beb96df4788d35864
Jake Ma40 <152456zh@gmail.com>
2022-08-13 13:16:00 +0500

    LazyLoad

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { ReactComponent as Next } from "../assets/icon-next.svg";
 import { ReactComponent as Previous } from "../assets/icon-previous.svg";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 function Slider({images}) {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -18,7 +19,7 @@ function Slider({images}) {
 
   return (
     <div className="slider">
-      <img className="slider-img" src={images[currentIndex].url} alt="prdouct1" style={{ maxWidth: "100%" }} />
+      <LazyLoadImage className="slider-img" src={images[currentIndex].url} alt="prdouct1" style={{ maxWidth: "100%" }} />
       <div className="btn-wrapper next">
         <Next onClick={goToNext} />
       </div>
